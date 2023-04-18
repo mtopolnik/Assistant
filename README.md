c48b1e9b0ce369db6615e36013a011dad1e79970
Marko Topolnik <marko.topolnik@gmail.com>
2023-04-18 10:02:59 +0200

    Add legal text

---
+++ README.md
@@ -0,0 +1,2 @@
+This project is licensed under the GNU General Public License v3.0 - see the 
+[LICENSE](LICENSE) file for details.
--- app/src/main/java/org/mtopol/assistant/Application.kt
+++ app/src/main/java/org/mtopol/assistant/Application.kt
@@ -1,11 +1,24 @@
+/*
+ * Copyright (C) 2023 Marko Topolnik
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.mtopol.assistant
 
 import android.app.Application
 import android.content.Context
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
