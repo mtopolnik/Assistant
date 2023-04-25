7287b55636aa6de76fce52ba65493877eddc0921
Marko Topolnik <marko.topolnik@gmail.com>
2023-04-25 16:15:29 +0200

    Finish System Prompt Fragment

--- app/src/main/java/org/mtopol/assistant/SystemPromptFragment.kt
+++ app/src/main/java/org/mtopol/assistant/SystemPromptFragment.kt
@@ -20,23 +20,52 @@ package org.mtopol.assistant
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuProvider
+import androidx.navigation.fragment.findNavController
 
-class SystemPromptFragment : Fragment() {
+class SystemPromptFragment : Fragment(), MenuProvider {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_system_prompt, container, false)
-        val edittextPrompt = view.findViewById<EditText>(R.id.editTextPrompt)
-        val buttonSave = view.findViewById<Button>(R.id.btnSave)
+        val activity = requireActivity() as AppCompatActivity
+        view.findViewById<Toolbar>(R.id.toolbar).also {
+            activity.setSupportActionBar(it)
+        }
+        activity.supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setTitle(R.string.title_edit_system_prompt)
+        }
+        activity.addMenuProvider(this, viewLifecycleOwner)
+
+        val edittextPrompt = view.findViewById<EditText>(R.id.edittext_system_prompt)
+        val buttonSave = view.findViewById<Button>(R.id.button_save_system_prompt)
         edittextPrompt.setText(requireContext().mainPrefs.systemPrompt)
         buttonSave.setOnClickListener {
             requireContext().mainPrefs.applyUpdate {
                 setSystemPrompt(edittextPrompt.text.toString())
             }
+            findNavController().popBackStack()
         }
         return view
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            findNavController().popBackStack()
+            return true
+        }
+        return false
     }
 }
