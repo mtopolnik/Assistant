cedbafc241601618616ebe1dbc456e6c5ae6e77c
Marko Topolnik <marko.topolnik@gmail.com>
2024-07-25 12:51:15 +0200

    Fine-tune prompt for summarization

--- app/src/main/java/org/mtopol/assistant/OpenAI.kt
+++ app/src/main/java/org/mtopol/assistant/OpenAI.kt
@@ -138,9 +138,9 @@ class OpenAI {
     }
 
     suspend fun summarizing(chat: List<Exchange>): String {
-        val systemPrompt = "Your task is summarizing a conversation between a User and an Assistant." +
-                " Your prompt will be the entire conversation, and you will respond with a very short" +
-                " summary, up to 30 characters in length. Do not add any other text except the summary itself."
+        val systemPrompt = "Your task is to create a title for a conversation between a User and an Assistant." +
+                " Your prompt will be the entire conversation, and you will respond with a title that summarizes" +
+                " it, up to 30 characters in length. Do not add any other text except the title itself."
 
         val text = chat.map { exchange ->
             "User:\n\n${exchange.promptText()}\n\nAssistant:\n\n${exchange.replyMarkdown}"
