610a0cff1f505d1984b4b1c4d31d6ba71b70143d
Marko Topolnik <marko.topolnik@gmail.com>
2024-09-30 14:18:58 +0200

    Revert "Remove sound amplifier"
    This reverts commit 61d6054ea06f78e47b3f3e73f62b08da3ca2b380.

--- app/src/main/java/org/mtopol/assistant/ChatFragment.kt
+++ app/src/main/java/org/mtopol/assistant/ChatFragment.kt
@@ -1000,6 +1000,7 @@ class ChatFragment : Fragment(), MenuProvider {
     @androidx.annotation.OptIn(UnstableApi::class)
     private suspend fun speakWithOpenAi(sentenceFlow: Flow<String>) {
         var lastValidVoice = appContext.mainPrefs.selectedVoice
+        var amplifier: LoudnessEnhancer? = null
         val exoPlayer = ExoPlayer.Builder(appContext)
             .setAudioAttributes(AudioAttributes.Builder()
                 .setUsage(C.USAGE_ASSISTANT)
@@ -1009,7 +1010,15 @@ class ChatFragment : Fragment(), MenuProvider {
                 .setBufferDurationsMs(BUFFER_MS, BUFFER_MS, START_PLAYBACK_WHEN_BUFFERED_MS, AFTER_UNDERRUN_REBUFFER_MS)
                 .setPrioritizeTimeOverSizeThresholds(true)
                 .build())
-            .build()
+            .build().apply {
+                addListener(object : Player.Listener {
+                    override fun onPlaybackStateChanged(state: Int) {
+                        if (state != Player.STATE_ENDED && amplifier == null) {
+                            amplifier = LoudnessEnhancer(audioSessionId).apply { setTargetGain(200); enabled = true }
+                        }
+                    }
+                })
+            }
         val speakLatch = CompletableDeferred<Unit>()
         if (!appContext.mainPrefs.isMuted) {
             Log.i("speech", "speakLatch.complete()")
@@ -1077,6 +1086,7 @@ class ChatFragment : Fragment(), MenuProvider {
             vmodel.muteToggledCallback = null
             exoPlayer.stop()
             exoPlayer.release()
+            amplifier?.release()
         }
     }
 
